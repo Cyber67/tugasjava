0bc82cb8c09523483828cf3bffc210a564691458
Cyber67 <68477133+Cyber67@users.noreply.github.com>
2020-07-19 14:28:20 +0700

    Add files via upload

---
+++ CYBER.sh
@@ -0,0 +1,41 @@
+#?/data/data/com.termux/files/usr/bin/bash
+
+
+#TOOL: SPAM SMS
+#LANGUAGE: Bash
+#author: CYBER CMEY7
+
+echo "[~] Selamat menggunakan Tool saya [~]"
+echo "[~] Gunakan dengan bijak [~]"
+echo "[~] Selamat menggunakn ( ͡° ͜ʖ ͡°) [~]"
+
+
+read -p "Masukan nomer target =" pillih
+read -p "Masukan jumlah spam =" pillih
+
+case $pillih in
+*)
+echo "Nomer Target Di temukan"
+sleep 8
+echo "Spam Sms Akan Di mulai 8 detik Lagi..."
+sleep 5
+echo "spam berhasil"
+sleep 2
+echo "spam berhasil"
+sleep 2
+echo "spam berhasil"
+sleep 2
+echo "spam berhasil"
+sleep 2
+echo "spam berhasil"
+sleep 2
+echo "spam berhasil"
+sleep 6
+echo "spam sukses"
+
+
+sleep 7
+echo "[~] thank you ( ͡° ͜ʖ ͡°) [~]"
+;;
+esac
+
